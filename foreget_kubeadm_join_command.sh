0dba9b6207ea928697a0ded55ef7280d266938ea
hujian <hujian@tigerft.com>
2018-10-17 16:32:51 +0800

    修改

--- foreget_kubeadm_join_command.sh
+++ foreget_kubeadm_join_command.sh
@@ -4,4 +4,4 @@
 token=$(kubeadm token list |grep -v "TOKEN"|awk '{print $1}')
 cert=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
 
-echo "kubeadm join --token $token --discovery-token-ca-cert-hash sha256:$cert 192.168.9.230:6443"
+echo "kubeadm join --token $token --discovery-token-ca-cert-hash sha256:$cert K8S_MASTER_IP:6443"
